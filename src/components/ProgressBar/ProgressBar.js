71a3a19d4f99ee63d84fd5bd6121eea0c02ae582
Guilherme Popolin <guilherme@hedvig.com>
2022-10-28 15:52:21 +0200

    refact(ProgressBar): final touches

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,92 +2,71 @@ import React from "react";
 import styled from "styled-components";
 
 import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const sizeStyles = {
+const barStylesBySize = {
   small: {
     "--height": "calc(8rem / 16)",
-    "--borderRadius": "4px",
+    "--margin": "0",
   },
   medium: {
     "--height": "calc(12rem / 16)",
-    "--borderRadius": "4px",
+    "--margin": "0",
   },
   large: {
-    "--height": "calc(24rem / 16)",
-    "--borderRadius": "8px",
+    "--height": "calc(16rem/ 16)",
+    "--margin": "4px",
   },
 };
 
-const progressbarLabelId = "progressbar-label";
+const wrapperStylesBySize = {
+  small: {
+    "--radius": "4px",
+    "--padding": "0",
+  },
+  medium: {
+    "--radius": "4px",
+    "--padding": "0",
+  },
+  large: {
+    "--radius": "8px",
+    "--padding": "4px",
+  },
+};
 
 const ProgressBar = ({ value = 0, size = "medium" }) => {
-  let ProgressBarComponent = null;
-  switch (size) {
-    case "large":
-      ProgressBarComponent = LargeProggressBar;
-      break;
-    case "small":
-      ProgressBarComponent = SmallProgressBar;
-      break;
-    case "medium":
-    default:
-      ProgressBarComponent = MediumgProgressBar;
-      break;
-  }
-
   return (
-    <Wrapper>
-      <strong id={progressbarLabelId}>{value}%</strong>
-      <ProgressBarComponent style={sizeStyles[size]} value={value} />
+    <Wrapper
+      style={wrapperStylesBySize[size]}
+      role="progressbar"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-valuenow={value}
+    >
+      <BarWrapper>
+        <Bar style={barStylesBySize[size]} value={value} />
+      </BarWrapper>
+      <VisuallyHidden>{value}%</VisuallyHidden>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div({
-  display: "flex",
-  alignItems: "center",
-  gap: "32px",
-});
-
-const BaseProgressBar = styled.span.attrs(({ value }) => ({
-  "aria-role": "progressbar",
-  "aria-valuemin": "0",
-  "aria-valuemax": "100",
-  "aria-valuenow": value,
-  "aria-labelledby": progressbarLabelId,
-}))(({ value }) => ({
-  position: "relative",
-  flex: 1,
-  backgroundColor: COLORS.transparentGray15,
-  width: "100%",
-  height: "var(--height)",
-  borderRadius: "var(--borderRadius)",
-  overflow: "hidden",
+  borderRadius: "var(--radius)",
+  padding: "var(--padding)",
   boxShadow: `inset 0px 2px 4px ${COLORS.transparentGray35}`,
-
-  "&::after": {
-    content: "''",
-    position: "absolute",
-    width: `${value}%`,
-    height: "100%",
-    backgroundColor: COLORS.primary,
-  },
-}));
-
-const SmallProgressBar = styled(BaseProgressBar)({});
-
-const MediumgProgressBar = styled(BaseProgressBar)({});
-
-const LargeProggressBar = styled(BaseProgressBar)({
-  padding: "4px",
-
-  "&::after": {
-    height: "calc(100% - 8px)",
-    top: "4px",
-    left: "4px",
-    borderTopLeftRadius: "4px",
-    borderBottomLeftRadius: "4px",
-  },
+  backgroundColor: COLORS.transparentGray15,
 });
+
+const BarWrapper = styled.div({
+  borderRadius: "4px",
+  overflow: "hidden",
+});
+
+const Bar = styled.div(({ value }) => ({
+  width: `${value}%`,
+  height: "var(--height)",
+  backgroundColor: COLORS.primary,
+}));
 
 export default ProgressBar;
